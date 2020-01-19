087509a1e46d9e1eb1fe2efcd4ddac1323b93f78
mc <dandford@gmail.com>
2020-01-19 13:42:46 +0800

    Update to fit new website

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,8 +5,7 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
-
-  # Do not eager load code on boot.
+  config.hosts.clear  # Do not eager load code on boot.
   config.eager_load = false
 
   # Show full error reports.
@@ -37,7 +36,6 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   # config/environments/production.rb
-
   config.action_mailer.delivery_method = :smtp
   host = 'localhost:3000'
   config.action_mailer.default_url_options = { :host => 'localhost:3000', protocol: 'http' }
